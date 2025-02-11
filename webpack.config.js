955d703ec24510ec4ab12e487c1992566a8e17d6
papiezonator <52249311+papiezonator@users.noreply.github.com>
2025-02-11 11:06:43 +0100

    added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,15 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: "./src/template.html",
+      template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
